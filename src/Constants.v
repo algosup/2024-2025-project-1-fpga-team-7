90a56c958bc51851ac9d2441f56a68c79066c0f8
Maxime THIZEAU <maxime.thizeau@algosup.com>
2024-10-10 11:46:22 +0200

    constants update
    Co-Authored-By: Tino GABET <182209255+Furimizu@users.noreply.github.com>

--- src/Constants.v
+++ src/Constants.v
@@ -1,44 +1,49 @@
 // This file will contain all the constants we made for the game.
 
 // Grid constants
-parameter TILE_SIZE             = 32;
-parameter X_TOTAL_TILE          = 20;
-parameter Y_TOTAL_TILE          = 15;
+parameter TILE_SIZE                     = 6'd32;
+parameter X_TOTAL_TILE                  = 5'd20;
+parameter Y_TOTAL_TILE                  = 4'd15;
 
 // Scoring contant
-parameter c_SCORE_INI           = 1;
+parameter c_SCORE_INI                   = 1'b1;
 
 // Player constants
-parameter c_X_BASE_POSITION     = 320;
-parameter c_Y_BASE_POSITION     = 384;
+parameter c_X_BASE_POSITION             = 9'd320;
+parameter c_Y_BASE_POSITION             = 9'd384;
+
+// States machine
+parameter IDLE                          = 1'b0;
+parameter RUNNING                       = 1'b1;
+
 
 // Obstacles constants
-parameter c_X_BASE_CAR_POSITION = 0;
+parameter c_X_BASE_CAR_POSITION         = 1'b0;
 parameter c_X_REVERSE_BASE_CAR_POSITION = 608;
-parameter c_Y_BASE_CAR_POSITION = 11;
-parameter c_BASE_CAR_SPEED = 781250;
-parameter c_LINE_4_Y = 320;
-parameter c_LINE_3_Y = 256;
-parameter c_LINE_2_Y = 192;
-parameter c_LINE_1_Y = 128;
-parameter c_NB_CARS = 4;
+parameter c_Y_BASE_CAR_POSITION         = 11;
+parameter c_BASE_CAR_SPEED              = 390625;
+parameter c_LINE_4_Y                    = 320;
+parameter c_LINE_3_Y                    = 256;
+parameter c_LINE_2_Y                    = 192;
+parameter c_LINE_1_Y                    = 128;
+parameter c_NB_CARS                     = 4;
 
 // VGA constants
-parameter H_VISIBLE_AREA    = 640;
-parameter H_FRONT_PORCH     = 16;                                                           // Unvisible Area
-parameter H_SYNC_PULSE      = 96;                                                           // Unvisible Area
-parameter H_BACK_PORCH      = 48;                                                           // Unvisible Area
-parameter H_TOTAL           = H_VISIBLE_AREA + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH;
+parameter H_VISIBLE_AREA                = 640;
+parameter H_FRONT_PORCH                 = 16;                                                           // Unvisible Area
+parameter H_SYNC_PULSE                  = 96;                                                           // Unvisible Area
+parameter H_BACK_PORCH                  = 48;                                                           // Unvisible Area
+parameter H_TOTAL                       = H_VISIBLE_AREA + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH;
 
-parameter V_VISIBLE_AREA    = 480;
-parameter V_FRONT_PORCH     = 10;
-parameter V_SYNC_PULSE      = 2;
-parameter V_BACK_PORCH      = 33;
-parameter V_TOTAL           = V_VISIBLE_AREA + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH;
+parameter V_VISIBLE_AREA                = 480;
+parameter V_FRONT_PORCH                 = 10;
+parameter V_SYNC_PULSE                  = 2;
+parameter V_BACK_PORCH                  = 33;
+parameter V_TOTAL                       = V_VISIBLE_AREA + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH;
 
 // Clock constaints 
-parameter c_DEBOUNCE_LIMIT  = 250000;
+parameter c_DEBOUNCE_LIMIT              = 250000;
 
-parameter COUNT_LIMIT       = 3125000;
+parameter COUNT_LIMIT                   = 3125000;
 
-parameter NUM_BITS          = 5;
+parameter NUM_BITS                      = 4;
